b00801ae875921ddb4cd95dedcc8480026924a8c
Emre <emre.coskun@bs.nttdata.com>
2023-06-23 09:50:59 +0200

    Course Controller Added

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddScoped<ICategoryService, CategoryService>(); //ICategoryService'i CategoryService ile eşleştiriyoruz.
+builder.Services.AddScoped<ICourseService, CourseService>(); //ICourseService'i CourseService ile eşleştiriyoruz.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
