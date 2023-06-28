5a635e1fe110d9193a982a133a08b5db78b70048
Emre <emre.coskun@bs.nttdata.com>
2023-06-28 15:07:52 +0200

    IdentityServer Added

---
+++ FreeCourse.IdentıtyServer/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using FreeCourse.IdentıtyServer.DTOs;
+using FreeCourse.IdentıtyServer.Models.IdentityTables;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.IdentıtyServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDTO signUpDTO)
+        {
+            var user = new AppUser
+            {
+                UserName = signUpDTO.UserName,
+                Email = signUpDTO.Email,
+                City = signUpDTO.City
+            };
+            var result = await _userManager.CreateAsync(user, signUpDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(_obj => _obj.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ FreeCourse.IdentıtyServer/DTOs/SignUpDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace FreeCourse.IdentıtyServer.DTOs
+{
+    public class SignUpDTO
+    {
+        public SignUpDTO(string userName, string email, string password, string city)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            City = city;
+        }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
