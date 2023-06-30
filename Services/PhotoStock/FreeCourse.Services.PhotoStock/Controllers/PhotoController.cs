19e5952436dc3475cdd51af4278b7c6650ee53fa
Emre <emre.coskun@bs.nttdata.com>
2023-06-30 14:20:42 +0200

    Conflict resolving

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotoController.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.Services.PhotoStock.DTOs;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> SavePhoto(IFormFile photo, CancellationToken cancellationToken) //cancellation token is used to cancel the request
+        {
+            if(photo == null || photo.Length <= 0)
+            {
+                return CreateActionResultInstance(Response<PhotoDTO>.Fail("Photo is not found", 400));
+            }
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            await photo.CopyToAsync(stream, cancellationToken); //cancellationToken is used to cancel the request if user closes the browser or something like that
+
+            var returnPath = "photos/" + photo.FileName;
+
+            PhotoDTO photoDTO = new() { Url = returnPath };
+
+            return CreateActionResultInstance(Response<PhotoDTO>.Success(photoDTO, 200));
+        }
+        [HttpPost]
+        [Route("delete")]
+        public IActionResult DeletePhoto(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            else
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo is not found", 404));
+            }
+
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
